1afdae3c61f74fc82f55ae52057f40fcd3ea8e71
raf-zig <ziravin@yandex.ru>
2023-02-02 19:06:44 +0500

    Fix the CodeMaker class

--- game.rb
+++ game.rb
@@ -1,10 +1,11 @@
 class CodeMaker
   all_colors = ['p','br','o','g','b','y']
-  computer_selected_colors = all_colors.sample(4) 
+  @computer_selected_colors = all_colors.sample(4) 
 
   def user_selected_colors
     puts 'Choose a color combination in the format "o g y b"'
-    user_input = gets.split
-    user_selection = user_input.split(' ')
+    @user_selection = gets.strip.split
   end
 end
+
+
