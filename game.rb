54694e440b68dc19f1ee52a0d7e22d61df281b08
raf-zig <ziravin@yandex.ru>
2023-02-03 15:24:39 +0500

    add the search_for_a_hidden_combination function

--- game.rb
+++ game.rb
@@ -8,11 +8,32 @@ class CodeMaker
   end
 end
 
+
 class CodeBreaker
   def initialize(selected_code)
     @selected_code = selected_code
   end
+  
+  @@guessed_combinations = []
 
-  choosing_a_computer = ['p','br','o','g','b','y'].sample(4)
+  @@choosing_a_computer = ['p','br','o','g','b','y'].sample(4)
+    
+  def search_for_a_hidden_combination
+    @selected_code.each_with_index do |element, index|
+      if @@choosing_a_computer.include? (element)
+        @@guessed_combinations[index] = element
+      end
+    end
+  end   
+   
+  def debugging
+    puts @selected_code
+    puts @@choosing_a_computer
+    puts @@guessed_combinations
+    puts @@guessed_combinations.size
+  end
   
 end
+q = CodeBreaker.new(['br','o','g','b'])
+q.search_for_a_hidden_combination
+puts q.debugging
