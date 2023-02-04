05b1fa420ab94b12f573e5d3a0cde826c9805cc8
raf-zig <ziravin@yandex.ru>
2023-02-04 23:00:14 +0500

    Add a loop interrupt to the search_for_a_hidden_combination method

--- user_code_breaker.rb
+++ user_code_breaker.rb
@@ -4,12 +4,12 @@ class UserCodeBreaker
   end
 
   def search_for_a_hidden_combination
-    3.times do
-      puts 'Enter a possible combination in the format "o g y b"'
-      @user_option = gets.strip.split
-
       @number_of_guessed_colors = []
       @number_of_complete_matches = []
+
+    12.times do
+      puts 'Enter a possible combination in the format "o g y b"'
+      @user_option = gets.strip.split
 
       @selected_code.each_with_index do |element, index| 
         if @user_option.include? (element)
@@ -18,18 +18,18 @@ class UserCodeBreaker
 
         if element == @user_option[index] 
           @number_of_complete_matches << element
-            if @number_of_complete_matches.length == 4
-              puts 'The user wined'
-              return
-            end
+          if @number_of_complete_matches.length == 4
+            puts 'The user wined'
+          end
         end
       end
-        puts "Number of guessed colors - #{@number_of_guessed_colors.length}"
-        puts "Number of complete matches - #{@number_of_complete_matches.length}"
-        @number_of_guessed_colors = []
-        @number_of_complete_matches = []
+      break if @number_of_complete_matches.length == 4
+      puts "Number of guessed colors - #{@number_of_guessed_colors.length}"
+      puts "Number of complete matches - #{@number_of_complete_matches.length}"
+      @number_of_guessed_colors = []
+      @number_of_complete_matches = []
     end 
-    puts "The user lost the game this time. The hidden combination - #{@selected_code}"
+    puts "The user lost the game this time. The hidden combination - #{@selected_code}" if @number_of_complete_matches.length != 4
   end
 end
 
