edc1b80041b0eede1f0c53d7d29519592ba9bf1f
raf-zig <ziravin@yandex.ru>
2023-02-04 22:23:20 +0500

    Add the cycle of the search_for_a_hidden_combination method

--- user_code_breaker.rb
+++ user_code_breaker.rb
@@ -4,23 +4,34 @@ class UserCodeBreaker
   end
 
   def search_for_a_hidden_combination
-    puts 'Enter a possible combination in the format "o g y b"'
-    @user_option = gets.strip.split
-  end
+    3.times do
+      puts 'Enter a possible combination in the format "o g y b"'
+      @user_option = gets.strip.split
 
-  @@number_of_guessed_colors = []
-  @@number_of_complete_matches = []
+      @number_of_guessed_colors = []
+      @number_of_complete_matches = []
 
-  @selected_code.each_with_index do |element, index| do
-    if @user_option.include? (element)
-      @@number_of_guessed_colors << element
-      puts "Number of guessed colors - #{@@number_of_guessed_colors.length}"
-    end
-    if element == @user_option[index] 
-        @@number_of_complete_matches << element
-        puts "Number of complete matches - #{@@number_of_complete_matches.length}"
-    end
+      @selected_code.each_with_index do |element, index| 
+        if @user_option.include? (element)
+          @number_of_guessed_colors << element
+        end
+
+        if element == @user_option[index] 
+          @number_of_complete_matches << element
+            if @number_of_complete_matches.length == 4
+              puts 'The user wined'
+              return
+            end
+        end
+      end
+        puts "Number of guessed colors - #{@number_of_guessed_colors.length}"
+        puts "Number of complete matches - #{@number_of_complete_matches.length}"
+        @number_of_guessed_colors = []
+        @number_of_complete_matches = []
+    end 
+    puts "The user lost the game this time. The hidden combination - #{@selected_code}"
   end
-  @@number_of_guessed_colors = []
-  @@number_of_complete_matches = []
 end
+
+q = UserCodeBreaker.new(['br','o','g','b'])
+q.search_for_a_hidden_combination
