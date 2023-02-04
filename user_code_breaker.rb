f5caef4ceadb9b5100cc1f588fa0f669ee2c3574
raf-zig <ziravin@yandex.ru>
2023-02-04 15:40:30 +0500

    Add a search guessed colors

--- user_code_breaker.rb
+++ user_code_breaker.rb
@@ -5,6 +5,16 @@ class UserCodeBreaker
 
   def search_for_a_hidden_combination
     puts 'Enter a possible combination in the format "o g y b"'
-    @user option = gets.strip.split
+    @user_option = gets.strip.split
+  end
+
+  @@number_of_guessed_colors = []
+  @@number_of_complete_matches = []
+
+  @selected_code.each_with_index do |element, index| do
+    if @user_option.include? (element)
+      @@number_of_guessed_colors << element
+      puts "Number of guessed colors - #{@@number_of_guessed_colors.length}"
+    end
   end
 end
