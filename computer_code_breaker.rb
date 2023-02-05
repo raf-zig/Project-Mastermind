b96f11ad97c5f4a7b96854fd71f1070781fffd3c
raf-zig <ziravin@yandex.ru>
2023-02-06 00:27:42 +0500

    Change the logic of the loop

--- computer_code_breaker.rb
+++ computer_code_breaker.rb
@@ -1,42 +1,42 @@
 class ComputerCodeBreaker
   def initialize(selected_code)
-    @selected_code = selected_code
+    @user_selected_code = selected_code
   end
 
   def search_a_hidden_combination
     @all_colors = ['p','br','o','g','b','y'] 
-    @guessed_colors  = []
     @complete_matches =Array.new(4)
-
+   
     12.times do
       @choosing_a_computer = @all_colors.sample(4)
-
-      @selected_code.each_with_index do |element, index|
-        if @choosing_a_computer.include? (element)
-          @guessed_colors[index] = element
-        end
+      
+      @user_selected_code.each_with_index do |element, index|
         if element == @choosing_a_computer[index] 
           @complete_matches[index] = element 
           if @all_colors.size > 4
             @all_colors.delete(element)
           else
-           @all_colors.map! {|x| x == element ? '*' : x}
+            @all_colors.map! {|x| x == element ? '*' : x}
+          end
+        else 
+          if @choosing_a_computer.include? (element)
+            @complete_matches[index] = element 
           end
         end
       end
-      print "Your code -                         #{@selected_code.join(', ')}\n"
-      print "A complete match in past attempts - #{@complete_matches.join(', ')}\n"
-      print "Computer code -                     #{@choosing_a_computer.join(', ')}\n\n"
-      
+      print "Your code -                         #{@user_selected_code.join(', ')}\n"
+      print "Computer code -                     #{@choosing_a_computer.join(', ')}\n"
+      print "A complete match in past attempts - #{@complete_matches.join(', ')}\n\n"
+   
       sleep(1)
 
-      if  @selected_code == @complete_matches
+      if  @user_selected_code == @complete_matches
         puts 'The computer winded'
         break
       end
 
     end
-    puts 'The computer lost the game this time.' if  @selected_code != @complete_matches
+    puts 'The computer lost the game this time.' if  @user_selected_code != @complete_matches
   end 
 end
 
--- user_code_breaker.rb
+++ user_code_breaker.rb
@@ -4,32 +4,32 @@ class UserCodeBreaker
   end
 
   def search_for_a_hidden_combination
-      @number_of_guessed_colors = []
-      @number_of_complete_matches = []
+      @guessed_colors = []
+      @complete_matches = []
 
     12.times do
       puts 'Enter a possible combination in the format "o g y b"'
       @user_option = gets.strip.split
 
       @selected_code.each_with_index do |element, index| 
-        if @user_option.include? (element)
-          @number_of_guessed_colors << element
+        if @user_option.include? element
+          @guessed_colors << element
         end
 
         if element == @user_option[index] 
-          @number_of_complete_matches << element
-          if @number_of_complete_matches.length == 4
+          @complete_matches << element
+          if @complete_matches.length == 4
             puts 'The user wined'
           end
         end
       end
-      break if @number_of_complete_matches.length == 4
-      puts "Number of guessed colors - #{@number_of_guessed_colors.length}"
-      puts "Number of complete matches - #{@number_of_complete_matches.length}"
-      @number_of_guessed_colors = []
-      @number_of_complete_matches = []
+      break if @complete_matches.length == 4
+      puts "Number of guessed colors - #{@guessed_colors.length}"
+      puts "Number of complete matches - #{@complete_matches.length}"
+      @guessed_colors = []
+      @complete_matches = []
     end 
-    puts "The user lost the game this time. The hidden combination - #{@selected_code}" if @number_of_complete_matches.length != 4
+    puts "The user lost the game this time. The hidden combination - #{@selected_code.join (' ')}" if @complete_matches.length != 4
   end
 end
 
