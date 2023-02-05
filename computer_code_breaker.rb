8d7c29b52dbc604c4aee598959001d01a0ec25cb
raf-zig <ziravin@yandex.ru>
2023-02-05 11:50:37 +0500

    Replace class variables with class instance variables

--- computer_code_breaker.rb
+++ computer_code_breaker.rb
@@ -2,46 +2,35 @@ class ComputerCodeBreaker
   def initialize(selected_code)
     @selected_code = selected_code
   end
-  
-  @@all_colors = ['p','br','o','g','b','y']
-  
-  @@guessed_combinations = []
-  @@guessed_combinations2 = []
 
   def search_a_hidden_combination
+    @all_colors = ['p','br','o','g','b','y'] 
+    @number_of_guessed_colors  = []
+    @number_of_complete_matches = []
+
     12.times do
-      @choosing_a_computer = @@all_colors.sample(4)
+      @choosing_a_computer = @all_colors.sample(4)
       @selected_code.each_with_index do |element, index|
         if @choosing_a_computer.include? (element)
-          @@guessed_combinations[index] = element
+          @number_of_guessed_colors[index] = element
         end
         if element == @choosing_a_computer[index] 
-          @@guessed_combinations2[index] = element 
-          if @@all_colors.size > 4
-            @@all_colors.delete(element)
+          @number_of_complete_matches[index] = element 
+          if @all_colors.size > 4
+            @all_colors.delete(element)
           else
-            @@all_colors.map! {|x| x == element ? '*' : element}
+            @all_colors.map! {|x| x == element ? '*' : element}
           end
         end
       end
       print "#{@selected_code}\n"
       print "#{@choosing_a_computer}\n"
-      print "#{@@guessed_combinations2}\n"
+      print "#{@number_of_complete_matches}\n"
     end
   end 
-  
-  def debugging
-    print "#{@selected_code}\n"
-    puts "#{@@choosing_a_computer}\n"
-    puts "#{@@guessed_combinations}\n"
-    puts "#{@@guessed_combinations2}\n"
-    puts "#{@@all_colors}\n"
-    puts @@guessed_combinations.size
-  end
-  
 end
 
-q = CodeBreaker.new(['br','o','g','b'])
+q = ComputerCodeBreaker.new(['br','o','g','b'])
 q.search_a_hidden_combination
- # puts q.debugging
+
  
